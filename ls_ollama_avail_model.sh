7eed2aebb37a40f53a2fd0bfa5490bf34ac9cc66
Fred DENIS <fred.denis3@gmail.com>
2024-06-28 13:14:05 +0200

    a simple script to list the ollama avaibale models

---
+++ ls_ollama_avail_model.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Fred DENIS -- 28-Jun-2024
+# List all ollama available models; indeed, I didnt find any commnand to do that; may be in the future: https://github.com/ollama/ollama/issues/2022
+#
+printf "%-25s: %s\n" "Model" "Description"
+for i in {1..120}; do printf "%s" "-"; done; printf "\n"
+curl https://ollama.com/library --silent | grep -E "/library/|max-w-md break-words" |\
+    awk -F "[\"/]" '{  if ($3 == "library")              {model=$4};
+                       if ($2 == "max-w-md break-words") {gsub(/[<>]/, "", $3);printf("%-25s: %s\n", model, $3) | "sort"}
+                    }'
+for i in {1..120}; do printf "%s" "-"; done; printf "\n"
