2bea328159b58866504e1e64f988717eb2baffcd
Fred DENIS <fred.denis3@gmail.com>
2024-06-29 15:07:59 +0200

    more doc

--- ls_ollama_avail_model.sh
+++ ls_ollama_avail_model.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Fred DENIS -- 28-Jun-2024
+# Fred DENIS -- 28-Jun-2024 -- https://unknowndba.blogspot.com/
 # List all ollama available models; indeed, I didnt find any commnand to do that; may be in the future: https://github.com/ollama/ollama/issues/2022
+# Script created to help in https://unknowndba.blogspot.com/2024/06/getting-started-with-ollama.html
 #
 printf "%-25s: %s\n" "Model" "Description"
 for i in {1..120}; do printf "%s" "-"; done; printf "\n"
